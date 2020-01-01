991cf99e173409c668ed8ba57abc2f6f98ecaebf
thomas marsh <Thomasmarsh16@gmail.com>
2019-12-31 19:04:00 -0500

    Add project files.

---
+++ aspNetCoreTicketSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspNetCoreTicketSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
