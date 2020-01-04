effbc8cbb0fc31d69b911159f5e182b7994d0bb4
thomas marsh <Thomasmarsh16@gmail.com>
2020-01-03 19:24:38 -0500

    add Model files for project

---
+++ aspNetCoreTicketSystem/Models/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspNetCoreTicketSystem.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public Boolean CompletedProj { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CompletionDate { get; set; }
+
+    }
+}
---
+++ aspNetCoreTicketSystem/Models/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace aspNetCoreTicketSystem.Models
+{
+    public class Worker
+    {
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; }
+        public string WorkerPosition { get; set; }
+        public string WorkerPhoneNum { get; set; }
+        public string WorkerEmail { get; set; }
+    }
+}
