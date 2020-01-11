d764efd7b324f8673caac6ec215f4486f3884f9f
thomas marsh <Thomasmarsh16@gmail.com>
2020-01-11 13:41:45 -0500

    add error page and worker controller

--- aspNetCoreTicketSystem/Controllers/HomeController.cs
+++ aspNetCoreTicketSystem/Controllers/HomeController.cs
@@ -52,7 +52,7 @@ namespace aspNetCoreTicketSystem.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
